ea4f18dc884f3e3e4e9484fcf4ab334770b53e47
Rasul <overlord.ove@mail.ru>
2022-10-27 22:49:56 +0300

    code cplitting

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <App/>
-)
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
