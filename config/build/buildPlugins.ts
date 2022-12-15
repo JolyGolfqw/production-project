e5120a8ae929bd3a24f55b81d659537f5f81815f
Rasul <overlord.ove@mail.ru>
2022-12-15 18:28:34 +0300

    plugins setting for prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,7 +8,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -21,9 +21,15 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       _IS_DEV_: JSON.stringify(isDev),
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      })
+    );
+  }
+  return plugins;
 }
